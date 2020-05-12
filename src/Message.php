303b5f1fee126b3dc1f9872326b5addd92a364b4
Junker <1144095+Junker@users.noreply.github.com>
2020-05-12 23:55:12 +0700

    add htmlBody(),rawBody(),headers() functions to Message

--- src/Message.php
+++ src/Message.php
@@ -27,16 +27,57 @@ class Message extends Model
     }
 
     /**
-     * Get the message body
+     * Get the HTML message body
      *
-     * @return string   The email body
+     * @return string   The HTML email body
      */
-    public function body()
+    public function htmlBody()
     {
-        $fullPathParts = explode("/", $this->html_path);
-        $partialPath = join("/", array_slice($fullPathParts, 3));
-        $this->body = (new Model())->get($partialPath)->getAttributes()[0];
-        return $this->body;
+        $partialPath = self::partialPath($this->html_path);
+        $this->html_body = static::$client->get($partialPath);
+        return $this->html_body;
+    }
+
+    /**
+     * Get the TEXT message body
+     *
+     * @return string   The TEXT email body
+     */
+    public function textBody()
+    {
+        $partialPath = self::partialPath($this->txt_path);
+        $this->txt_body = static::$client->get($partialPath);
+        return $this->txt_body;
+    }
+
+    /**
+     * Get the RAW message body
+     *
+     * @return string   The RAW email body
+     */
+    public function rawBody()
+    {
+        $partialPath = self::partialPath($this->raw_path);
+        $this->raw_body = static::$client->get($partialPath);
+
+        return $this->raw_body;
+    }
+
+    /**
+     * Get the message header
+     *
+     * @return array   The headers
+     */
+    public function headers()
+    {
+        return static::$client->get('inboxes/'.$this->inbox_id.'/messages/'.$this->id.'/mail_headers');
+    }
+
+
+    private static function partialPath($path)
+    {
+        $fullPathParts = explode("/", $path);
+        return join("/", array_slice($fullPathParts, 3));
     }
 
 
